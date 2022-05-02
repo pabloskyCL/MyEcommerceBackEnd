f92985b875672863ffc207dbb6bb0f7e3a3fa52d
pabloskyCL <pabloskiquiroz@gmail.com>
2022-05-02 18:49:53 -0400

    improvement: agrega login de usuarios con validación de usuario mediante JWT
    - se agrega en AuthController.php el action profile() con ruta `register/profile`, el cual valida si el usuario esta actualmente autenticado, retornando un arreglo con el usuario authenticado y la variable isAuth.
    
    - se agrega ruta de login en routes.yaml para validar las credenciales de usuario al hacer login de forma externa.
    
    - se actualiza configuración security.yaml con la ruta creada para validar credenciales de usuario en routes.yaml se indica el campo con el que se validara el usuario en la cofiguración, por esta ruta se autentica el usuario de forma externa.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -30,23 +30,6 @@ class AuthController extends AbstractController
     }
 
     /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
-
-    /**
      *@Route("/register", name="user.register")
      *
      */
@@ -55,17 +38,25 @@ class AuthController extends AbstractController
         $jsonData = json_decode($request->getContent());
         $user = $this->userRepository->create($jsonData);
 
-
         return new JsonResponse([
             'user' => $this->serializer->serialize($user,'json')
-        ]);
+        ],201);
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/profile", name="user.profile")
+     *
      */
-    public function logout(): void
+    public function profile(): JsonResponse
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $isAuthenticated = true;
+        $currentUser = $this->security->getUser();
+        if(!$currentUser){
+           $isAuthenticated = false;
+        }
+        $user = $this->serializer->serialize($currentUser, 'json');
+
+        return new JsonResponse(['user'=> $user,
+            'isAuth'=>$isAuthenticated],200);
     }
 }
